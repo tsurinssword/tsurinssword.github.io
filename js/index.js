2c6d3bced7670a974f1458f377ce30dbab56567d
Harveer Singh Bassi <tsurinssword@gmail.com>
2022-01-29 23:03:09 +0000

    Initial commit
    Copied site over from FTP

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+  $('.chart').easyPieChart({
+    scaleColor: "#ecf0f1",
+    lineWidth: 20,
+    lineCap: 'butt',
+    barColor: '#4682B4',
+    trackColor:	"#ecf0f1",
+    size: 160,
+    animate: 500
+  });
+$('.newChart').easyPieChart({
+    scaleColor: "#ecf0f1",
+    lineWidth: 20,
+    lineCap: 'butt',
+    barColor: '#ff9800',
+    trackColor: "#ecf0f1",
+    size: 160,
+    animate: 500
+});
+});
